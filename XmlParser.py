1c98f8267ec55515ad1f9f869ae9623976f6153e
Hanka <hrosicka@post.cz>
2024-03-12 22:08:45 +0100

    Renamed keys in dictionary

--- XmlParser.py
+++ XmlParser.py
@@ -19,6 +19,16 @@ party_list = data["my_party_list"]
 # Iterating through the json list
 for i in data['my_party_list']:
     print(i["first_name"] + "\t" + i["last_name"] + "\t" + i["phone"] + "\t" + i["email"])
+    # rename keys in dictionary
+    i["First Name"] = i["first_name"]
+    del i["first_name"]
+    i["Last Name"] = i["last_name"]
+    del i["last_name"]
+    i["Phone"] = i["phone"]
+    del i["phone"]
+    i["Email"] = i["email"]
+    del i["email"]
+
 
 # Creating Excel Writer Object from Pandas  
 writer = pd.ExcelWriter('JsonParserPython\\party_list.xlsx',engine='xlsxwriter')   
